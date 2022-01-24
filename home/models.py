186411bce86f776d70e25fc6a9da51fea00a149f
anandhteja yaragopu <anandhteja32@gmail.com>
2022-01-25 02:04:08 +0530

    changes jan24th

--- home/models.py
+++ home/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from cloudinary_storage.storage import MediaCloudinaryStorage, RawMediaCloudinaryStorage, VideoMediaCloudinaryStorage
+
 
 
 
@@ -34,7 +36,7 @@ class Profile(models.Model):
 class Post(models.Model):
     username=models.CharField(max_length=100)
     photos=models.ImageField(upload_to='photos', null=True,blank=True, validators=[validate_image])
-    videos=models.FileField(upload_to='videos', null=True,blank=True,)
+    videos=models.FileField(upload_to='videos', null=True,blank=True,storage=VideoMediaCloudinaryStorage(),)
     video_description=models.TextField(max_length=1000, null=True,blank=True)
     description=models.TextField(max_length=1000, null=True,blank=True)
     status=models.TextField(max_length=1000, null=True,blank=True)
--- instaclone/settings.py
+++ instaclone/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -157,3 +160,4 @@ CLOUDINARY_STORAGE = {
     
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'  
+DEFAULT_FILE_STORAGE  ='cloudinary_storage.storage.VideoMediaCloudinaryStorage'
