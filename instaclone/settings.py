7ef7bd0fb8953fa047fc29e3f7df92fc87bf8cdb
anandhteja yaragopu <anandhteja32@gmail.com>
2022-01-30 23:26:10 +0530

    changesjan30th

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ewv664&anwa)+5&t!ruys88!c&f_ni#489fmqvx943m9+^b!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tej-socialmedia.herokuapp.com']
 
 
 # Application definition
@@ -86,21 +86,22 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+
 DATABASES = {
 
     'default': {
 
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'instagram',
+        'NAME': 'dds1cci2aeqgpb',
 
-        'USER': 'root',
+        'USER': 'nvrqsvmfixsikq',
 
-        'PASSWORD': 'pass@143',
+        'PASSWORD': 'b28ddc3ee01f9cf18ca522b6a6985efd9495c23a177b85a8d04ccaee11585e5a',
 
-        'HOST': 'localhost',
+        'HOST': 'ec2-54-208-139-247.compute-1.amazonaws.com',
 
-        'PORT': '3306',
+        'PORT': '5432',
 
     }
 
