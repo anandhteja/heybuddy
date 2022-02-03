103441254fd1deef4656ac3dbd020eb8c4ac18ab
anandhteja yaragopu <anandhteja32@gmail.com>
2022-02-03 20:17:29 +0530

    changes feb3rd

--- home/views.py
+++ home/views.py
@@ -48,7 +48,8 @@ def home(request):
            
               
             po=Post.objects.all().order_by('-uploaded_on')
-            f=list(Follow.objects.all().filter(follower=v).values_list('following',flat=True))
+            fo=list(Follow.objects.all().filter(follower=v).values_list('following',flat=True))
+            f=fo.append(v)
             private=list(Privateaccount.objects.all().values_list('username',flat=True))
 
 
@@ -74,8 +75,8 @@ def discover(request):
            
               
             po=Post.objects.all().order_by('-uploaded_on')
-            fo=list(Follow.objects.filter(follower=v).values_list('following',flat=True))
-            f=fo.append(v)
+            f=list(Follow.objects.filter(follower=v).values_list('following',flat=True))
+            
 
             private=list(Privateaccount.objects.all().values_list('username',flat=True))
 
